0ebeae3dbc9f3ea4293959d4371bb230bd434af4
Mike Lynch <52801418+mikelynch97@users.noreply.github.com>
2019-09-20 16:01:52 +0000

    AWESOME JOB

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import { tipCalculation } from './math';
 
-const txtBillAmount = document.getElementById('txtBillAmount') as HTMLInputElement;
-const btnTipPercent = document.getElementById('btnTipPercent') as HTMLButtonElement;
+const entry = document.getElementById('entry') as HTMLInputElement;
+const output = document.getElementById('output') as HTMLInputElement;
+const tip10 = document.getElementById('tip10') as HTMLInputElement;
+const tip15 = document.getElementById('tip15') as HTMLInputElement;
+const tip20 = document.getElementById('tip20') as HTMLInputElement;
 
 const txtTipAmount = document.getElementById('txtTipAmount') as HTMLDivElement;
 const sumBillAmount = document.getElementById('sumBillAmount') as HTMLUListElement;
@@ -9,19 +12,51 @@ const sumTipPercentage = document.getElementById('sumTipPercentage') as HTMLULis
 const sumAmountOfTip = document.getElementById('sumAmountOfTip') as HTMLUListElement;
 const sumTotalToBePaid = document.getElementById('sumTotalToBePaid') as HTMLUListElement;
 
+let tipPercent = .10;
+
 export function runApp() {
-    txtBillAmount.addEventListener('input', function () {
-        const sum = tipCalculation(txtBillAmount.valueAsNumber, .1);
-
-        txtTipAmount.innerText = `You are tipping 10%`;
-        sumBillAmount.innerText = `Bill Amount: $${txtBillAmount.valueAsNumber.toString()}.00`;
-        sumTipPercentage.innerText = `Tip Percentage: 10%`;
-        sumAmountOfTip.innerText = `Amount of tip: $${sum}.00`;
-        sumTotalToBePaid.innerHTML = `Total to be Paid: $${sum + txtBillAmount.valueAsNumber}.00`;
+    entry.addEventListener('keydown', function (evt) {
+        if (evt.key === 'e' || evt.key === 'E' || evt.key === '-' || evt.key === '+') {
+            evt.preventDefault();
+        }
+        calc();
     });
-
-
+    tip10.addEventListener('click', () => {
+        tipPercent = .10;
+        calc();
+        tip10.disabled = true;
+        tip15.disabled = false;
+        tip20.disabled = false;
+    });
+    tip15.addEventListener('click', () => {
+        tipPercent = .15;
+        calc();
+        tip10.disabled = false;
+        tip15.disabled = true;
+        tip20.disabled = false;
+    });
+    tip20.addEventListener('click', () => {
+        tipPercent = .20;
+        calc();
+        tip10.disabled = false;
+        tip15.disabled = false;
+        tip20.disabled = true;
+    });
 }
 
+function calc() {
+    if (entry.valueAsNumber < 0) {
+        entry.classList.add('error');
+        output.innerText = 'ERROR!!';
+    } else {
+        const tip = (Math.round((entry.valueAsNumber * tipPercent) * Math.pow(10, 2)) / Math.pow(10, 2)).toFixed(2);
 
-
+        txtTipAmount.innerText = 'You are tipping ' + (tipPercent * 100) + '%';
+        sumTipPercentage.innerText = 'Tip Percentage: ' + (tipPercent * 100) + '%';
+        sumBillAmount.innerText = 'Bill Amount: $' + entry.valueAsNumber;
+        sumAmountOfTip.innerText = 'Amount of tip: $' + tip.toString();
+        sumTotalToBePaid.innerHTML = `Total to be Paid: $${(entry.valueAsNumber * tipPercent + entry.valueAsNumber).toString()}.00`;
+        entry.classList.remove('error');
+        output.innerText = '';
+    }
+}
